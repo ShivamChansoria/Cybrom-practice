20aec84077fffb17935a0b21a7600a385b6689eb
ShivamChansoria <chansoriyashivam@gmail.com>
2024-06-28 18:47:57 +0530

    Added  Comment and Formated the file.

--- 28-06-ii.cpp
+++ 28-06-ii.cpp
@@ -1,3 +1,4 @@
+//**********************************Function for Adding Bonus to Salary by using Function *******************************
 #include<iostream>
 using namespace std;
 
@@ -6,13 +7,14 @@ int main(){
     float bonus=0;
     cout<<"GIve your salary and bonus%: ";
     cin>>salary>>bonus;
-    float empSal(int, float);
-    int total=empSal(salary, bonus);
-    cout<<"Your salary and with the bonus is:"<<total<<endl;
+    float empSal(int, float);//1.Function Declarations
+    int total=empSal(salary, bonus);//3. Function Call
+    cout<<"Your salary and with the bonus is:"<<total<<"Rs."<<endl;
 return 0;
 }
 
-float empSal(int sal, float per){
+float empSal(int sal, float per){//2.Function Definitions
+
     float bonus=sal*(per/100);
     cout<<"Your bonus are:"<<bonus<<endl;
     float total=sal+bonus;
