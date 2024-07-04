b2fbb884116f3cce12537ce975a2d209f211d07f
ShivamChansoria <chansoriyashivam@gmail.com>
2024-07-04 19:00:41 +0530

    Union& Classes

---
+++ 04-07-ii.cpp
@@ -0,0 +1,8 @@
+//************************************** Classes in C++ ************************************************
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+return 0;
+}
---
+++ 04-07.cpp
@@ -0,0 +1,19 @@
+// *********************************************** Union in C++ ******************************************* 
+#include<iostream>
+using namespace std;
+
+union book{
+    int page;
+    char name[8];//Allotes the size of 8 bytes to a object.
+    float price;
+};
+
+int main(){
+    
+    book b1;
+    b1.page=80;
+    b1.price=122.99;
+    cout << sizeof(b1) << endl;
+    cout<< b1.page << endl; // Will show the garbage value becuase the attributes in Unions are not 'Reusable'.
+return 0;
+}
